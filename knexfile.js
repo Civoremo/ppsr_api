a6717631530efa7262aa0ecb1350dd76a81a34ca
Civoremo <omerovic_n@hotmail.com>
2019-10-17 15:15:37 -0400

    env change

--- knexfile.js
+++ knexfile.js
@@ -11,7 +11,7 @@ const localPg = {
 const pg = require("pg");
 pg.defaults.ssl = false;
 
-const dbConnection = process.env.DB_URL;
+const dbConnection = process.env.DATABASE_URL;
 
 module.exports = {
 	development: {
